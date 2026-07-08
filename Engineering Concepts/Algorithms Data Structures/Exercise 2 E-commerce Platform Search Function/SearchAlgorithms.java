8d1b083cf98c6c3f8853c923b612a2716723dd22
riya040204 <rp.riyapatel20@gmail.com>
2026-07-08 23:50:04 +0530

    docs: add comments explaining search algorithms

--- Engineering Concepts/Algorithms Data Structures/Exercise 2 E-commerce Platform Search Function/SearchAlgorithms.java
+++ Engineering Concepts/Algorithms Data Structures/Exercise 2 E-commerce Platform Search Function/SearchAlgorithms.java
@@ -1,31 +1,3 @@
 public class SearchAlgorithms {
-
-    public static Product linearSearch(Product[] products, int targetId) {
-        for (int i = 0; i < products.length; i++) {
-            if (products[i].productId == targetId) {
-                System.out.println("Linear Search: Found at index " + i);
-                return products[i];
-            }
-        }
-        System.out.println("Linear Search: Product not found");
-        return null;
-    }
-
-    public static Product binarySearch(Product[] products, int targetId) {
-        int left = 0;
-        int right = products.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (products[mid].productId == targetId) {
-                System.out.println("Binary Search: Found at index " + mid);
-                return products[mid];
-            } else if (products[mid].productId < targetId) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        System.out.println("Binary Search: Product not found");
-        return null;
-    }
+    
 }
